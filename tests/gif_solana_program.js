4dd7c59c135100437527f81b1679dba30e7e5c48
civoremo <civoremon@gmail.com>
2021-11-06 21:33:19 -0400

    initial standard solana program

---
+++ tests/gif_solana_program.js
@@ -0,0 +1,55 @@
+/** @format */
+
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("\nStart test...\n");
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.GifSolanaProgram;
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  console.log("Transaction signature\n");
+  console.log("-------------------------\n");
+  console.log(tx, "\n");
+  console.log("-------------------------\n\n");
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("Gif Count ---- ", account.totalGifs.toString());
+
+  await program.rpc.addGif("insert_a_giphy_link_here", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("Gif Count ---- ", account.totalGifs.toString());
+
+  console.log("Gif List ---- ", account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
